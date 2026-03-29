ad4426a6a0cc8ef8030d9c9b1b26336fa576f224
Marley <marley.nodejs@gmail.com>
2026-03-29 03:54:58 +0300

    Update imdb_dag.py

--- Chapter10 - Building a Big Data Pipeline on Kubernetes/batch/dags/imdb_dag.py
+++ Chapter10 - Building a Big Data Pipeline on Kubernetes/batch/dags/imdb_dag.py
@@ -8,8 +8,8 @@ from datetime import datetime
 import requests
 import boto3
 
-// aws_access_key_id=Variable.get("aws_access_key_id")
-// aws_secret_access_key=Variable.get("aws_secret_access_key")
+# aws_access_key_id=Variable.get("aws_access_key_id")
+# aws_secret_access_key=Variable.get("aws_secret_access_key")
 
 aws_access_key_id='admin'
 aws_secret_access_key='password'
